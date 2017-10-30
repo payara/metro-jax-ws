f854bf0a3f186251d77fc11d32376d8cc2e1bc44
merorai <susan.rai04@gmail.com>
2017-10-30 10:40:32 +0000

    webservices-osgi 2.3.2-b608.payara-p3

--- jaxws-ri/rt/src/main/java/com/sun/xml/ws/transport/Headers.java
+++ jaxws-ri/rt/src/main/java/com/sun/xml/ws/transport/Headers.java
@@ -103,18 +103,16 @@ public class Headers extends TreeMap<String,List<String>> {
         }
     }
     
+    // case-sensitive string comparison of HTTP header names.
     private static final class SensitiveComparator implements Comparator<String>, Serializable {
 
         public int compare(String o1, String o2) {
-            if ((o1 == null) && (o2 == null)) {
+            if ((o1 == null) && (o2 == null))
                 return 0;
-            }
-            if (o1 == null) {
+            if (o1 == null) 
                 return -1;
-            }
-            if (o2 == null) {
+            if (o2 == null) 
                 return 1;
-            }
             return o1.compareTo(o2);
         }
     }
