bf01d3c42f65ba5108cb584715cbfbee60814af8
Andrew Pielage <andrew.pielage@payara.fish>
2021-04-16 15:13:01 +0100

    Fix HeadersTest
    Signed-off-by: Andrew Pielage <andrew.pielage@payara.fish>

--- jaxws-ri/rt/src/test/java/com/sun/xml/ws/transport/HeadersTest.java
+++ jaxws-ri/rt/src/test/java/com/sun/xml/ws/transport/HeadersTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007, 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2007, 2019 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Distribution License v. 1.0, which is available at
@@ -22,31 +22,57 @@ import junit.framework.TestCase;
  */
 public class HeadersTest extends TestCase {
 
-    public void testputAllMap() throws Exception {
+    public void testputAllMapCaseInsensitive() throws Exception {
+        List<String> set_cookie = new ArrayList(1);
+        set_cookie.add("cookie_lowercase");
+        List<String> Set_Cookie = new ArrayList(1);
+        Set_Cookie.add("Cookie_CamelCase");
+        List<String> SET_COOKIE = new ArrayList(1);
+        SET_COOKIE.add("COOKIE_UPPERCASE");
 
-	List<String> set_cookie = new ArrayList(1);
-	set_cookie.add("cookie_lowercase");
-	List<String> Set_Cookie = new ArrayList(1);
-	Set_Cookie.add("Cookie_CamelCase");
-	List<String> SET_COOKIE = new ArrayList(1);
-	SET_COOKIE.add("COOKIE_UPPERCASE");
-	
-	
-	Map<String, List<String>> map = new HashMap<String, List<String>> ();
-	map.put("set-cookie",set_cookie);
-	map.put("Set-Cookie",Set_Cookie);
-	map.put("SET-COOKIE",SET_COOKIE);
-	
-	Headers out = new Headers();
-	out.putAll(map);
-        assertEquals(out.getFirst("set-cookie"),out.getFirst("Set-Cookie"));
-        assertEquals(out.getFirst("Set-Cookie"),out.getFirst("SET-COOKIE"));
-        assertEquals(out.getFirst("SET-COOKIE"),out.getFirst("set-cookie")); 
-        assertEquals(out.size(),1);
-        assertEquals(out.get("set-cookie").size(),3);
-	assertTrue(out.get("set-cookie").contains("cookie_lowercase"));
-	assertTrue(out.get("Set-Cookie").contains("Cookie_CamelCase"));
-	assertTrue(out.get("SET-COOKIE").contains("COOKIE_UPPERCASE"));
+
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+        map.put("set-cookie", set_cookie);
+        map.put("Set-Cookie", Set_Cookie);
+        map.put("SET-COOKIE", SET_COOKIE);
+
+        Headers out = new Headers(false);
+        out.putAll(map);
+        assertEquals(out.getFirst("set-cookie"), out.getFirst("Set-Cookie"));
+        assertEquals(out.getFirst("Set-Cookie"), out.getFirst("SET-COOKIE"));
+        assertEquals(out.getFirst("SET-COOKIE"), out.getFirst("set-cookie"));
+        assertEquals(out.size(), 1);
+        assertEquals(out.get("set-cookie").size(), 3);
+        assertTrue(out.get("set-cookie").contains("cookie_lowercase"));
+        assertTrue(out.get("Set-Cookie").contains("Cookie_CamelCase"));
+        assertTrue(out.get("SET-COOKIE").contains("COOKIE_UPPERCASE"));
     }
-	
-	}
+
+    public void testputAllMapCaseSensitive() throws Exception {
+        List<String> set_cookie = new ArrayList(1);
+        set_cookie.add("cookie_lowercase");
+        List<String> Set_Cookie = new ArrayList(1);
+        Set_Cookie.add("Cookie_CamelCase");
+        List<String> SET_COOKIE = new ArrayList(1);
+        SET_COOKIE.add("COOKIE_UPPERCASE");
+
+
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+        map.put("set-cookie", set_cookie);
+        map.put("Set-Cookie", Set_Cookie);
+        map.put("SET-COOKIE", SET_COOKIE);
+
+        // Default is case sensitive
+        Headers out = new Headers();
+        out.putAll(map);
+        assertNotSame(out.getFirst("set-cookie"), out.getFirst("Set-Cookie"));
+        assertNotSame(out.getFirst("Set-Cookie"), out.getFirst("SET-COOKIE"));
+        assertNotSame(out.getFirst("SET-COOKIE"), out.getFirst("set-cookie"));
+        assertEquals(out.size(), 3);
+        assertEquals(out.get("set-cookie").size(), 1);
+        assertTrue(out.get("set-cookie").contains("cookie_lowercase"));
+        assertTrue(out.get("Set-Cookie").contains("Cookie_CamelCase"));
+        assertTrue(out.get("SET-COOKIE").contains("COOKIE_UPPERCASE"));
+    }
+
+}
